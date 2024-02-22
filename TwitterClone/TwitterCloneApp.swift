c6c8f9298191f6a9934b24fc7f356754a0a0317b
Oluwayomi Felix <77457879+Portia-dot@users.noreply.github.com>
2024-02-21 22:55:44 -0600

    Initial Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Modamori Oluwayomi on 2024-02-21.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
